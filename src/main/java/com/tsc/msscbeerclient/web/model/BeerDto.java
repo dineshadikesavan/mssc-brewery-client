58784476997c8f800732a19ebe6f08d685387ab5
Dinesh A <dinesh.adikesavan@gmail.com>
2024-11-30 20:40:04 -0600

    Bean Validation and Error handling

--- src/main/java/com/tsc/msscbeerclient/web/model/BeerDto.java
+++ src/main/java/com/tsc/msscbeerclient/web/model/BeerDto.java
@@ -1,5 +1,8 @@
 package com.tsc.msscbeerclient.web.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,11 +18,18 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    @Null
     private UUID id;
     private Integer version;
     private OffsetDateTime createdDate;
+
+    @NotBlank
     private String beerName;
+
+    @NotBlank
     private String beerStyle;
+
+    @Positive
     private Long upc;
     private BigDecimal price;
     private Integer quantityOnHand;
